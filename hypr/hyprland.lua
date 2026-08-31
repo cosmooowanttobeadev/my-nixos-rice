cba1a3b39302e84d56c199de14985410a1c22a4c
Aadidev V Nair <226132850+cosmooowanttobeadev@users.noreply.github.com>
2026-08-31 20:04:52 +0530

    Update hyprland.lua

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -313,6 +313,10 @@ hl.bind("XF86AudioPause", hl.dsp.exec_cmd("playerctl play-pause"), { locked = tr
 hl.bind("XF86AudioPlay",  hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
 hl.bind("XF86AudioPrev",  hl.dsp.exec_cmd("playerctl previous"),   { locked = true })
 
+--Wlogout
+
+hl.bind("SUPER + Escape" , hl.dap.exec_cmd("wlogout"))
+
 --------------------------------
 ---- WINDOWS AND WORKSPACES ----
 --------------------------------
